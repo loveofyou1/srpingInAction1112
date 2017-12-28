077c917576082e91671eb9fb6add06005924ca89
sunlei <sunwenlei9@126.com>
2017-12-28 08:29:24 +0800

    spring in action 学习第四章

---
+++ src/main/java/chapter04/service/Audience4.java
@@ -0,0 +1,20 @@
+package chapter04.service;
+
+public class Audience4 {
+
+    public void silenceCellPhone() {
+        System.out.println("silence cellphone");
+    }
+
+    public void testSuides() {
+        System.out.println("Taking Seats");
+    }
+
+    public void applaud() {
+        System.out.println("CLAP CLAP CLAP");
+    }
+
+    public void demandRefund() {
+        System.out.println("Demanding Refund!");
+    }
+}
