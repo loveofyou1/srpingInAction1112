caddc195aa6285952cbf7e5bb2b4f24d848263ad
sunlei19 <sunlei19@jd.com>
2018-01-05 20:24:28 +0800

    第五章  springMvc详解，增加controller类

---
+++ src/main/java/chapter05/config/SpitterWebAppInitializer.java
@@ -0,0 +1,17 @@
+package chapter05.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpitterWebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{RootConfig.class};
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
